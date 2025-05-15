0f65ebc53917e67167e8429ab3887cc82984d822
Sandesh Sawant <s_sandesh21@yahoo.com>
2025-05-14 22:45:57 -0400

    new test

--- tests/pages/home_page.py
+++ tests/pages/home_page.py
@@ -5,27 +5,54 @@ class HomePage:
         self.page = page
         self.supplier_link = page.get_by_role("link", name="Suppliers")
         self.category_link = page.get_by_role("link", name="Categories")
-        self.addProduct_button = page.locator("button[name='Add Product']")
+        self.addProduct_button = page.get_by_role("button", name="Add Product")
+        self.productname_input = page.locator("#name")
+        self.quantity_input = page.locator("#quantity")
+        self.price_input = page.locator("#price")
+        self.supplier_input = page.locator("#supplier")
+        self.category_input = page.locator("#category")
+        self.barcode_input = page.locator("#barCode")
+        self.producttable = page.locator("table.w-full")
 
-       # self.password_input = page.locator("#password")
-       # self.login_button = page.locator("button[type='submit']")
-       # self.close_button = page.locator("button:has-text('Close modal')")
         self.logout_link = page.get_by_role("link", name="Logout")
-
 
     def goto(self):
         self.page.goto("http://localhost")
 
-    # def login(self, username: str, password: str):
-    #     self.username_input.fill(username)
-    #     self.password_input.fill(password)
-    #     self.page.wait_for_timeout(1000)
-    #     self.login_button.click()
-    #     self.page.wait_for_timeout(1000)
-    #     self.close_button.click()
-    #     self.page.wait_for_timeout(1000)
-
-
-    def home_click_addproduct(self):
+    def click_addproduct(self):
         self.addProduct_button.click()
         self.page.wait_for_timeout(1000)
+
+    def add_productname(self, productname: str):
+        self.productname_input.fill(productname)
+        self.page.wait_for_timeout(1000)
+
+    def add_quantity(self, quantity_num: str):
+        self.quantity_input.fill(quantity_num)
+        self.page.wait_for_timeout(1000)
+
+    def add_price(self, price_num: str):
+        self.price_input.fill(price_num)
+        self.page.wait_for_timeout(1000)
+
+    def select_supplier(self, suppliername: str):
+        self.supplier_input.select_option("#supplier", label=suppliername)
+        self.page.wait_for_timeout(1000)
+
+    def select_category(self, bookname: str):
+        self.category_input.select_option("#category", label=bookname)
+        self.page.wait_for_timeout(1000)
+
+    def add_barcode(self, barcode: str):
+        self.barcode_input.fill(barcode)
+        self.page.wait_for_timeout(1000)
+
+    def click_addnewproduct(self):
+        self.addProduct_button.click()
+        self.page.wait_for_timeout(1000)
+
+    def verify_productadded(self):
+        assert self.page.locator("table.w-full >> text=Test 1").count() > 0, "Product not found"
+        self.page.wait_for_timeout(1000)
+
+
--- tests/test_cases/test_addProduct.py
+++ tests/test_cases/test_addProduct.py
@@ -1,19 +1,22 @@
 import pytest
-from tests.pages.login_page import LoginPage
 from tests.pages.home_page import HomePage
 
+#assumption is user already landed on home page
 def test_add_product(page):
-    login_page = LoginPage(page)
-    login_page.goto()
-    login_page.login_fill_username("fabandalm")
-    login_page.login_fill_password("123456")
-    login_page.login_click_signin()
-    assert page.url == "http://localhost/stock"
+    home_page = HomePage(page)
+    home_page.click_addproduct()
+    home_page.add_productname("Product1")
+    home_page.add_quantity("5")
+    home_page.add_price("100")
+    home_page.select_supplier("Gotham Supplies")
+    home_page.select_category("Books")
+    home_page.add_barcode("121212")
+    home_page.click_addnewproduct()
+    home_page.verify_productadded()
 
-# def test_invalid_login(page):
-#     login_page = LoginPage(page)
-#     login_page.goto()
-#     login_page.login("invalid", "invalid")
-#     # page.wait_for_selector("p:has-text('There was an error processing your request.')")
-#     # error_message = page.locator("p:has-text('There was an error processing your request.')").text_content()
-#     assert "There was an error processing your request." in "There was an error processing your request."
+
+
+
+
+
+
